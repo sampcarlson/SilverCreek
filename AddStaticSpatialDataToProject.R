107401ef7f07e3d115c34cd74014146248c74f41
sampcarlson <sam.p.carlson@gmail.com>
2022-03-29 15:22:25 -0600

    reproject network to 4326 before export

--- AddStaticSpatialDataToProject.R
+++ AddStaticSpatialDataToProject.R
@@ -3,8 +3,6 @@ library(sf)
 netDSN="C:/Users/sam/Documents/spatial/SilverCreek/SilverCreekNet.gpkg"
 netName="SilverCreek"
 
-
-
 #load data
 network=st_read(dsn=netDSN,layer=netName)
 
@@ -13,5 +11,9 @@ network=st_zm(network)
 
 plot(network$geom,col="blue")
 
+
+##################Leaflet app is in 4326.  Reproject for easier use there:
+
+st_transform(network,crs=st_crs(4326))
 #save to .rds file for simple access
-saveRDS(network,file="C:/Users/sam/Documents/R/projects/SilverCreekApp/network.rds")
+saveRDS(network,file="C:/Users/sam/Documents/SilverCreek/R/SilverCreekApp/network.rds")
